7447129090c8ba191d32b15bf70edc077da4ffc5
vuhung05 <nguyenvuhung1996@gmail.com>
2018-10-01 22:41:32 +0700

    Chạm màn hình phát hiện khuôn mặt

---
+++ app/src/main/java/com/example/vuhung/facedetectmlkit/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.vuhung.facedetectmlkit;
+
+
+
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.example.vuhung.facedetectmlkit.Helper.GraphicOverlay;
+import com.example.vuhung.facedetectmlkit.Helper.RectOverlay;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView ;
+    GraphicOverlay graphicOverlay;
+    AlertDialog waitingDialog;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //init view
+        cameraView  = (CameraView)findViewById(R.id.camera_view);
+        graphicOverlay = (GraphicOverlay)findViewById(R.id.graphic_overlay);
+        waitingDialog = new SpotsDialog.Builder().setContext(this)
+                .setMessage("Vui lòng đợi")
+                .setCancelable(false)
+                .build();
+        graphicOverlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.start();
+                cameraView.captureImage();
+                graphicOverlay.clear();
+            }
+        });
+
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                waitingDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(), cameraView.getHeight(),false);
+                //cameraView.stop();
+
+                runFaceDetector(bitmap);
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+    }
+
+    private void runFaceDetector(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage .fromBitmap(bitmap);
+
+        FirebaseVisionFaceDetectorOptions options = new FirebaseVisionFaceDetectorOptions.Builder().build();
+
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(options);
+
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+
+                        if (firebaseVisionFaces.size()==0) waitingDialog.dismiss();
+                        processFaceResult(firebaseVisionFaces);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void processFaceResult(List<FirebaseVisionFace> firebaseVisionFaces) {
+        int count = 0;
+        for (FirebaseVisionFace face: firebaseVisionFaces) {
+            //Rect bounds = face.getBoundingBox();
+            //Draw rectangle
+            //RectOverlay rect = new RectOverlay(graphicOverlay, bounds);
+            //graphicOverlay.add(rect);
+            waitingDialog.dismiss();
+            count ++;
+        }
+            Toast.makeText(this,String.format("Có %d khuôn mặt trong ảnh",count),Toast.LENGTH_SHORT).show();
+
+
+    }
+
+}
